71ff3c87a5f5d240287225522ac01d8baf4ccdaa
GOSIjnr <71373171+GOSIjnr@users.noreply.github.com>
2025-04-30 16:56:51 +0100

    patch v1
    - ensured all imported code is working as expected

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -4,7 +4,15 @@ extends Node
 var isToastActive: bool = false
 var selectedGameResource: Games
 
-@export var PQColor: Array[Color]
+@export var PQColor: Array[Color] = [
+	Color("#2BBFA9"),
+	Color("#EF2430"),
+	Color("#EC1A61"),
+	Color("#9C29A8"),
+	Color("#E79220"),
+	Color("#69A0FB"),
+]
+
 
 func _ready() -> void:
 	Engine.max_fps = 60
